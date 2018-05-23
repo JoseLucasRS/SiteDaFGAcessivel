0ad4ec8abcf0606c5a2e5a0ac0a605eb0b863bfa
Lucas <jlucasrsantana@gmail.com>
2018-05-23 09:17:45 -0300

    Arquivos de css, js e fontes armazenados localmente devido a problemas de CORS

---
+++ css/style.php
@@ -0,0 +1,198 @@
+.mtli_attachment {
+	display:inline-block;
+	height: auto;
+	min-height:24px;
+	background-position: top left;
+	background-attachment: scroll;
+	background-repeat: no-repeat;
+	padding-left: 28px !important;
+}
+.mtli_3g2 {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/3g2-icon-24x24.png); }
+.mtli_3gp {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/3gp-icon-24x24.png); }
+.mtli_ai {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/ai-icon-24x24.png); }
+.mtli_air {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/air-icon-24x24.png); }
+.mtli_asf {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/asf-icon-24x24.png); }
+.mtli_avi {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/avi-icon-24x24.png); }
+.mtli_bib {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/bib-icon-24x24.png); }
+.mtli_cls {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/cls-icon-24x24.png); }
+.mtli_csv {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/csv-icon-24x24.png); }
+.mtli_deb {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/deb-icon-24x24.png); }
+.mtli_djvu {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/djvu-icon-24x24.png); }
+.mtli_dmg {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/dmg-icon-24x24.png); }
+.mtli_doc {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/doc-icon-24x24.png); }
+.mtli_docx {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/docx-icon-24x24.png); }
+.mtli_dwf {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/dwf-icon-24x24.png); }
+.mtli_dwg {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/dwg-icon-24x24.png); }
+.mtli_eps {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/eps-icon-24x24.png); }
+.mtli_epub {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/epub-icon-24x24.png); }
+.mtli_exe {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/exe-icon-24x24.png); }
+.mtli_f {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/f-icon-24x24.png); }
+.mtli_f77 {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/f77-icon-24x24.png); }
+.mtli_f90 {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/f90-icon-24x24.png); }
+.mtli_flac {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/flac-icon-24x24.png); }
+.mtli_flv {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/flv-icon-24x24.png); }
+.mtli_gif {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/gif-icon-24x24.png); }
+.mtli_gz {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/gz-icon-24x24.png); }
+.mtli_ico {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/ico-icon-24x24.png); }
+.mtli_indd {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/indd-icon-24x24.png); }
+.mtli_iso {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/iso-icon-24x24.png); }
+.mtli_jpg {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/jpg-icon-24x24.png); }
+.mtli_jpeg {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/jpeg-icon-24x24.png); }
+.mtli_key {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/key-icon-24x24.png); }
+.mtli_log {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/log-icon-24x24.png); }
+.mtli_m4a {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/m4a-icon-24x24.png); }
+.mtli_m4v {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/m4v-icon-24x24.png); }
+.mtli_midi {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/midi-icon-24x24.png); }
+.mtli_mkv {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/mkv-icon-24x24.png); }
+.mtli_mov {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/mov-icon-24x24.png); }
+.mtli_mp3 {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/mp3-icon-24x24.png); }
+.mtli_mp4 {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/mp4-icon-24x24.png); }
+.mtli_mpeg {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/mpeg-icon-24x24.png); }
+.mtli_mpg {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/mpg-icon-24x24.png); }
+.mtli_msi {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/msi-icon-24x24.png); }
+.mtli_odp {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/odp-icon-24x24.png); }
+.mtli_ods {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/ods-icon-24x24.png); }
+.mtli_odt {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/odt-icon-24x24.png); }
+.mtli_oga {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/oga-icon-24x24.png); }
+.mtli_ogg {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/ogg-icon-24x24.png); }
+.mtli_ogv {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/ogv-icon-24x24.png); }
+.mtli_pdf {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/pdf-icon-24x24.png); }
+.mtli_png {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/png-icon-24x24.png); }
+.mtli_pps {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/pps-icon-24x24.png); }
+.mtli_ppsx {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/ppsx-icon-24x24.png); }
+.mtli_ppt {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/ppt-icon-24x24.png); }
+.mtli_pptx {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/pptx-icon-24x24.png); }
+.mtli_psd {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/psd-icon-24x24.png); }
+.mtli_pub {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/pub-icon-24x24.png); }
+.mtli_py {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/py-icon-24x24.png); }
+.mtli_qt {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/qt-icon-24x24.png); }
+.mtli_ra {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/ra-icon-24x24.png); }
+.mtli_ram {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/ram-icon-24x24.png); }
+.mtli_rar {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/rar-icon-24x24.png); }
+.mtli_rm {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/rm-icon-24x24.png); }
+.mtli_rpm {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/rpm-icon-24x24.png); }
+.mtli_rtf {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/rtf-icon-24x24.png); }
+.mtli_rv {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/rv-icon-24x24.png); }
+.mtli_skp {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/skp-icon-24x24.png); }
+.mtli_spx {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/spx-icon-24x24.png); }
+.mtli_sql {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/sql-icon-24x24.png); }
+.mtli_sty {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/sty-icon-24x24.png); }
+.mtli_tar {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/tar-icon-24x24.png); }
+.mtli_tex {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/tex-icon-24x24.png); }
+.mtli_tgz {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/tgz-icon-24x24.png); }
+.mtli_tiff {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/tiff-icon-24x24.png); }
+.mtli_ttf {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/ttf-icon-24x24.png); }
+.mtli_txt {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/txt-icon-24x24.png); }
+.mtli_vob {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/vob-icon-24x24.png); }
+.mtli_wav {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/wav-icon-24x24.png); }
+.mtli_wmv {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/wmv-icon-24x24.png); }
+.mtli_xls {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/xls-icon-24x24.png); }
+.mtli_xlsx {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/xlsx-icon-24x24.png); }
+.mtli_xml {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/xml-icon-24x24.png); }
+.mtli_xpi {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/xpi-icon-24x24.png); }
+.mtli_zip {
+	background-image: url(/web/20170603212457im_/http://faculdadeguanambi.edu.br/wp-content/plugins/mimetypes-link-icons/images/zip-icon-24x24.png); }
+/*
+     FILE ARCHIVED ON 21:24:57 Jun 03, 2017 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 11:27:05 May 23, 2018.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  LoadShardBlock: 678.466 (3)
+  esindex: 0.015
+  captures_list: 715.797
+  CDXLines.iter: 19.256 (3)
+  PetaboxLoader3.datanode: 1382.899 (4)
+  exclusion.robots: 0.183
+  exclusion.robots.policy: 0.169
+  RedisCDXSource: 9.84
+  PetaboxLoader3.resolve: 526.538 (2)
+  load_resource: 1242.686
+*/
